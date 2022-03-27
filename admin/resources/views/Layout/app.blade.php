49059f582371b0cdcc421352c8594bd03ee263c9
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-27 14:18:12 +0600

    laravel portfolio website start

---
+++ admin/resources/views/Layout/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield('content')</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/mdb.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/sidenav.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+    <link rel="stylesheet" href="{{asset('css/datatables.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/datatables-select.min.css')}}">
+</head>
+
+<body>
+
+ @yield('content') 
+ 
+
+
+
+
+
+
+<script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+<script type="{{asset('text/javascript')}}" src="js/popper.min.js"></script>
+<script type="{{asset('text/javascript')}}" src="js/bootstrap.js"></script>
+<script type="{{asset('text/javascript')}}" src="js/mdb.min.js"></script>
+<script src="{{asset('js/jquery.slimscroll.js')}}"></script>
+<script src="{{asset('js/sidebarmenu.js')}}"></script>
+<script src="{{asset('js/sticky-kit.min.js')}}"></script>
+<script src="{{asset('js/custom.min-2.js')}}"></script>
+<script src="{{asset('js/datatables.min.js')}}"></script>
+<script src="{{asset('js/datatables-select.min.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+<script src="{{asset('js/axios.min.js')}}"></script>   
+</body>
+</html>
+
+{{asset('')}}
